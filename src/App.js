6fb565d572c1321141738e3b34ed759abf06c52a
Aadarsh Singh <aadarsh4492@gmail.com>
2024-10-07 19:02:18 +0530

    Update Message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Website under maintenance</h1>
+      <p>Building awesome things for you - Magnet Tech Pvt Ltd</p>
     </div>
   );
 }
